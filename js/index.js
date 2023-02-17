abf5c56fbf2f8470498ae69586895dacceca0ab0
MehediHasan4321 <126mehedi@gmail.com>
2023-02-17 15:15:09 +0600

    update some UI/UX

--- js/index.js
+++ js/index.js
@@ -4,3 +4,30 @@ document.getElementById("shoping-icon").addEventListener("click",()=>{
 document.getElementById("crose-icon").addEventListener("click",()=>{
     document.getElementById("slider").style.width = `0px`
 })
+
+
+document.getElementById("card-one").addEventListener("click",()=>{
+    const productHeading =getInnerText("card-1-heading")
+    const productPrice = getInnerText("card-1-price")
+    const productQuentity = 1;
+    const totalPrice = parseInt(productPrice) * productQuentity;
+    vouser(productHeading,productPrice,productQuentity,totalPrice)
+})
+
+function vouser(productHeading,productPrice,productQuentity,totalPrice){
+    const ol = document.createElement("ol");
+    ol.innerHTML = `
+    <li>${1}</li>
+    <li class="text-lg font-semibold">${productHeading}</li>
+    <li>${productPrice}</li>
+    <li>${productQuentity}</li>
+    <li>${totalPrice}</li>`
+    ol.classList.add("ol-list")
+    document.getElementById("parent").appendChild(ol)
+}
+
+function getInnerText (id){
+    const elementText = document.getElementById(id).innerText
+
+    return elementText
+}
