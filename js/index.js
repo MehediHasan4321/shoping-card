3c8c64d7fd6184b4951876aa1e4899322992a29e
MehediHasan4321 <126mehedi@gmail.com>
2023-02-17 11:30:41 +0600

    UI/UX complete

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementById("shoping-icon").addEventListener("click",()=>{
+    document.getElementById("slider").style.width = `550px`
+})
+document.getElementById("crose-icon").addEventListener("click",()=>{
+    document.getElementById("slider").style.width = `0px`
+})
